63254ab169eccca79deefc1a7236c7ba76b32c20
Paulemberg Lima Silva <paulemberg@gmail.com>
2020-02-25 16:06:26 -0300

    feat: Login at instagram working

---
+++ app/__main__.py
@@ -0,0 +1,20 @@
+import time
+import json
+
+from selenium import webdriver
+
+
+with open('../config/configuration.json') as config_file:
+    data = json.load(config_file)
+
+driver = webdriver.Chrome('/home/paulemberg/projects/chromedriver_linux64/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('username')
+search_box.send_keys(data["user"])
+search_box = driver.find_element_by_name('password')
+search_box.send_keys(data["password"])
+
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+#driver.quit()
